bd7e3940a24d2de92c06963cc8c36ab3d98a76d0
andy <mramjon@gmail.com>
2023-01-17 23:13:57 +0000

    Initial Setup of basic quote commands

--- bot.py
+++ bot.py
@@ -13,7 +13,7 @@ GUILD = env.GUILD
 
 # Initialising bot basics
 intents = discord.Intents().all()
-bot = commands.Bot(command_prefix=["ZZZ ", "ZZz ", "Zzz ", "zzz ", "z.", "Z."], intents = discord.Intents().all())
+bot = commands.Bot(command_prefix=["ZZZ ", "ZZz ", "Zzz ", "zzz ", "z", "Z", "z.", "Z."], intents = discord.Intents().all())
 
 
 # First thing the bot does on launch
@@ -24,26 +24,101 @@ async def on_ready():
 # Bot looping
 
 # Bot commands
+
+# Quote command
 @bot.command(aliases = ["q"])
-async def quote(ctx, arg = ""):
-    with open("quoteList.json") as quoteDB:
-        quoteList = json.load(quoteDB)
-    if arg == "":
-        user = random.choice(list(quoteList))
-        randomNum = random.randint(0, len(quoteList[user]) - 1)
-        await ctx.send("\""+str(quoteList[user][randomNum]).capitalize()+"\" - " + str(user).capitalize())
-    elif arg != "":
-        if arg not in quoteList:
-            await ctx.send("That person isn't in the list.")
-            return
-        desired_quote = random.choice(list(quoteList[arg]))
-        await ctx.send("\""+str(desired_quote).capitalize() + "\"")
+async def quote(ctx, *, user_name: str = None):
+    with open("quotes.json", "r") as quoteDB:
+        data = json.load(quoteDB)
+        if user_name:
+            for person in data["users"]:
+                if user_name in person["aliases"]:
+                    quotes = person["quotes"]
+                    if len(quotes)>0:
+                        randQuote = random.choice(quotes)
+                        await ctx.send(f'"{randQuote.capitalize()}" - {person["aliases"][0]}')
+                    else:
+                        await ctx.send(f"Sorry, {person['aliases'][0]} doesn't have any quotes.")
+                    return
+            await ctx.send(f"Sorry, {user_name} not found.")
+        else:
+            user = data["users"]
+            if len(user)>0:
+                person = random.choice(user)
+                quotes = person["quotes"]
+                if len(quotes)>0:
+                    randQuote = random.choice(quotes)
+                    await ctx.send(f'"{randQuote.capitalize()}" - {person["aliases"][0]}')
+                else:
+                    await ctx.send(f"Sorry, {person['aliases'][0]} doesn't have any quotes.")
+            else:
+                await ctx.send("Sorry, no user found.")
+
+@bot.command(aliases = ["aq"])
+async def add_quote(ctx, user_name: str, *, quote: str = None):
+    if quote is None:
+        await ctx.send("Please provide a quote.")
+        return
+    with open("quotes.json", "r") as quoteDB:
+        data = json.load(quoteDB)
+    for person in data["users"]:
+        if user_name in person["aliases"]:
+            id = person["aliases"][0]
+            if quote in person["quotes"]:
+                await ctx.send("That quote already exists.")
+                return
+            else:
+                person["quotes"].append(quote)
+                with open("quotes.json", "w") as quoteDB:
+                    json.dump(data, quoteDB)
+                await ctx.send(f"Quote \"{quote}\" has been added to {person['aliases'][0]}!")
+                with open("lastQuote.json", "w") as lastQDB:
+                    json.dump(quote, lastQDB)
+                return
+    await ctx.send(f"Sorry, {user_name} not found.")
+
+@bot.command(aliases = ["lq", "last q"])
+async def last_quote(ctx):
+    with open("lastQuote.json", "r") as lastQDB:
+        lastQuote = json.load(lastQDB)
+    if lastQuote != "":
+        await ctx.send(f"The last quote added was: \"{lastQuote}\"")
+    else:
+        await ctx.send("No quotes have been added yet.")
+
+@bot.command(aliases=["liq", "list q"])
+async def list_quotes(ctx, *, user_name: str = None):
+    with open("quotes.json", "r") as quoteDB:
+        data = json.load(quoteDB)
+    if user_name is None:
+        await ctx.send("Please choose a person to list quotes from.")
+    else:
+        for person in data["users"]:
+            if user_name in person["aliases"]:
+                quotes = person["quotes"]
+                if len(quotes)>0:
+                    return_message = ""
+                    for i in range(len(quotes)):
+                        quote = "["+str(i + 1)+"] " + "\"" + quotes[i]+"\"" + "\n"
+                        if len(return_message + quote) > 1800:
+                            await ctx.send(str(return_message))
+                            return_message = quote
+                        else:
+                            return_message += quote
+                    await ctx.send(str(return_message))
+                    return
+        await ctx.send(f"Sorry, {user_name} not found.")
+
+@bot.command(aliases=["b", "bday"])
+async def birthday(ctx, *, user_name: str = None):
+    with open ("quotes.json", "r") as birthdayDB:
+        data = json.load()
 
 # Error handling    
 @bot.event
 async def on_command_error(ctx, error):
     if isinstance(error, CommandNotFound):
-        await ctx.send(f"That command doesn't exist.. yet? Ask and ye shall (maybe) receive! \n ||(Or use help)||")
+        await ctx.send(f"Either the command wasn't used correctly.. || Or something is broken - (use 'Zzz help') ||")
         return
     raise error
 

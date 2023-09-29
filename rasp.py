f7e4fa2035e33c8af81538183afbcf3b22b4180f
maland1 <mramjon@gmail.com>
2023-09-29 19:35:54 +0100

    resolving indent issues

--- rasp.py
+++ rasp.py
@@ -3,26 +3,26 @@ import subprocess
 class rasp:
 
 	def update_bot():
-	    try:
-		# Run the git pull command for the "release" branch
-		process = subprocess.Popen(
-		    ["git", "pull", "origin", "release"],
-		    stdout=subprocess.PIPE,
-		    stderr=subprocess.PIPE,
-		    universal_newlines=True
-		)
+		try:
+			# Run the git pull command for the "release" branch
+			process = subprocess.Popen(
+				["git", "pull", "origin", "release"],
+				stdout=subprocess.PIPE,
+				stderr=subprocess.PIPE,
+				universal_newlines=True
+			)
 
-		# Capture the standard output and error output
-		stdout, stderr = process.communicate()
+			# Capture the standard output and error output
+			stdout, stderr = process.communicate()
 
-		# Check if the pull was successful
-		if process.returncode == 0:
-		    print("Pull from 'release' branch was successful.")
-		else:
-		    print(f"Pull from 'release' branch failed with error:\n{stderr}")
+			# Check if the pull was successful
+			if process.returncode == 0:
+				print("Pull from 'release' branch was successful.")
+			else:
+				print(f"Pull from 'release' branch failed with error:\n{stderr}")
 
-	    except Exception as e:
-		print(f"An error occurred: {str(e)}")
+		except Exception as e:
+			print(f"An error occurred: {str(e)}")
 		
 	def restart_bot():
 		# restart pi

82d62d384deb1d38e560f915da3edd0b49a0a5ce
maland1 <mramjon@gmail.com>
2024-01-05 22:19:09 +0000

    fixing env file issues and removing import

--- bot.py
+++ bot.py
@@ -1,6 +1,5 @@
 from ast import alias
 import json
-import env
 import random
 import os
 import datetime
@@ -10,12 +9,14 @@ from discord.ext import commands, tasks
 from discord.ext.commands import CommandNotFound
 
 # Logging in and setting currently active servers
-TOKEN = env.TOKEN
-GUILD = env.GUILD
+with open("env.json", "r") as env:    
+    data = json.load(env)
+    TOKEN = data.get("secret_key")
+    GUILD = data.get("guilds")
+
 
 # Initialising bot basics
-intents = discord.Intents().all()
-bot = commands.Bot(command_prefix=["ZZZ ", "ZZz ", "Zzz ", "zzz ", "z.", "Z."], intents)
+bot = commands.Bot(command_prefix=["zzz "], intents = discord.Intents().all())
 
 
 # First thing the bot does on launch

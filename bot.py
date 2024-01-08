cacd20eab13653f8356d44161e1755498156ff36
Andrew Malcolm O'Neill <105134723+maland1@users.noreply.github.com>
2024-01-08 14:39:49 +0000

    Final fixes for birthday messages (#5)
    * Final fixes for formatting of birthday message

--- bot.py
+++ bot.py
@@ -161,8 +161,9 @@ async def birthday(ctx, *, userID: str = None):
                 birthday_str = name.get("birthday", "")
 
                 if birthday_str:
-                    birthday = datetime.strptime(birthday_str, "%m/%d").replace(year=today.year).date()
 
+                    birthday = datetime.strptime(birthday_str, "%d/%m").replace(year=today.year).date()
+                    
                     # Check if the birthday is in the future
                     if birthday >= today:
                         days_until_birthday = (birthday - today).days
@@ -178,10 +179,12 @@ async def birthday(ctx, *, userID: str = None):
             for name in data["users"]:
 
                 if userID in name["aliases"]:
-                    birthday = name["birthday"]
 
-                    if birthday != "":
-                        await ctx.send(f"{name['aliases'][0]}'s birthday is on {birthday}.")
+                    birthday_date = name.get("birthday", "")
+                    
+                    if birthday_date:
+                        birthday = datetime.strptime(birthday_date, "%d/%m").date()
+                        await ctx.send(f"{name['aliases'][0]}'s birthday is on the {birthday.strftime('%#dth of %B')}.")
 
                     else:
                         await ctx.send(f"Sorry, {name['aliases'][0]} doesn't have a birthday.")
